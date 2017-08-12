772735aed65f0a55326855679a0312d70a260fc1
aFrontendDev <grumpydev22@gmail.com>
2017-08-12 16:19:23 +0100

    adding in webpack style and basic js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,55 @@
+
+// variables
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// webpack config
 module.exports = {
-    entry: './assets/_scripts/main.js',
-    output: {
-        filename: './bundle.js'
-    }
+  entry: ['./assets/_scripts/main.js', './assets/_styles/main.scss'],
+  output: {
+    path: path.resolve('dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { // regular css files
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      { // scss files
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'dist/[name].styles.css',
+      allChunks: true,
+    })
+  ]
 };
